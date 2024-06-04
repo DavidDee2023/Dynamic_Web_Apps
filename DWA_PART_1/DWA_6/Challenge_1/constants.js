8da14b43400d4d45452b30f09eb8624c10d7d0d0
DavidDee2023 <d.nkosi@lab.clicklearning.org>
2024-06-04 12:16:46 +0200

    Added Constants.js file with it's data

---
+++ DWA_PART_1/DWA_6/Challenge_1/constants.js
@@ -0,0 +1,34 @@
+// constants.js
+
+export const SELECTORS = {
+
+    DATA_HEADER_SEARCH: '[data-header-search]',
+    DATA_HEADER_SETTINGS: '[data-header-settings]',
+    DATA_LIST_ITEMS: '[data-list-items]',
+    DATA_LIST_MESSAGE: '[data-list-message]',
+    DATA_LIST_BUTTON: '[data-list-button]',
+    DATA_LIST_ACTIVE: '[data-list-active]',
+    DATA_LIST_BLUR: '[data-list-blur]',
+    DATA_LIST_IMAGE: '[data-list-image]',
+    DATA_LIST_TITTLE: '[data-list-title]',
+    DATA_LIST_SUBTITLE: '[data-list-subtitle]',
+    DATA_LIST_DESCRIPTION: '[data-list-description]',
+    DATA_LIST_CLOSE: '[data-list-close]',
+    DATA_SEARCH_OVERLAY: '[data-search-overlay]',
+    DATA_SEARCH_FORM: '[data-search-form]',
+    DATA_SEARCH_TILTTLE: '[data-search-title]',
+    DATA_SEARCH_GENRES: '[data-search-genres]',
+    DATA_SEARCH_AUTHORS: '[data-search-authors]',
+    DATA_SEARCH_CANCEL: '[data-search-cancel]',
+    DATA_SETTINGS_OVERLAY: '[data-settings-overlay]',
+    DATA_SETTINGS_FORM: '[data-settings-form]',
+    DATA_SETINGS_THEME: '[data-settings-theme]',
+    DATA_SETTINGS_CANCEL: '[data-settings-cancel]',
+
+
+};
+
+export const CSS = {
+    DAY: ['255, 255, 255', '10, 10, 20'],
+    NIGHT: ['10, 10, 20', '255, 255, 255'],
+};
